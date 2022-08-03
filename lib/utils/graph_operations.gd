5e4581e0bf1a71057dabcc961a2f4776215b09ff
Laura <marylaura0205@gmail.com>
2022-08-03 04:22:08 -0300

    [first implementation of Twin Reduction in the pkg]
    	- In the utils directory about 4 new functions were
    	implemented in their first stage of development.
    	- Further testing of the Cokernel functionality is necessary

--- lib/utils/graph_operations.gd
+++ lib/utils/graph_operations.gd
@@ -3,4 +3,7 @@
 ## this section of the code make available certain necessary graphs methods
 ##
 ## code author: M. Laura da Silva
+DeclareGlobalFunction("GraphTwins"); 
+DeclareGlobalFunction("IsTwinFree"); 
 DeclareGlobalFunction("TwinReduction"); 
+DeclareGlobalFunction("CokernelGraph"); 
